3e9721d0bd3cda811ead4965b5569983c8a5bfdf
Daniel Rojas <danielrc14@gmail.com>
2024-04-25 21:59:28 -0400

    feat: service for the rick and morty api

---
+++ src/services/rick_and_morty_api_service.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+export default class RickAndMortyApiService {
+  constructor () {
+    this.axiosInstance = axios.create({
+      baseURL: 'https://rickandmortyapi.com/api'
+    })
+  }
+
+  async getCharacters (page = 1) {
+    const response = await this.axiosInstance.get('/character', {
+      params: {
+        page: page
+      }
+    })
+    const mappedResults = response.data.results.map(character => {
+      return {
+        name: character.name,
+        status: character.status,
+        species: character.species,
+        originName: character.origin.name
+      }
+    })
+    return {
+      results: mappedResults,
+      hasNext: response.data.info.next !== null
+    }
+  }
+}
