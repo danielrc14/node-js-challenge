fa0549061c91e17c985ad43e36ea61e071803cb7
Daniel Rojas <danielrc14@gmail.com>
2024-04-26 00:34:54 -0400

    feat: method for getting at most N characters

--- src/controllers/character_controller.js
+++ src/controllers/character_controller.js
@@ -1,11 +1,39 @@
 import models from '../models'
 import BaseController from './base'
+import RickAndMortyApiService from '../services/rick_and_morty_api_service'
 
 export default class CharacterController extends BaseController {
   CharacterController () { }
 
   async index (req, res) {
-    return super.Success(res, '')
+    const N = req.query.N || 20
+    const rickAndMortyApiService = new RickAndMortyApiService()
+
+    let page = 1
+    let totalAmount = 0
+    const characters = []
+    while (totalAmount < N) {
+      const data = await rickAndMortyApiService.getCharacters(page)
+
+      const pageCharacters = data.results
+      characters.push(...pageCharacters)
+
+      if (!data.hasNext) {
+        break
+      }
+
+      totalAmount += pageCharacters.length
+      page += 1
+    }
+
+    if (characters.length > N) {
+      characters.length = N
+    }
+
+    return super.Success(res, {
+      results: characters,
+      count: characters.length
+    })
   }
 
   async create (req, res) {
