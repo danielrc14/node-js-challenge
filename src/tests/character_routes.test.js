8ebce51dde192cc5cb19eb424cb46f507a1981e7
Daniel Rojas <danielrc14@gmail.com>
2024-04-27 11:55:09 -0400

    chore: tests for the second endpoint

--- src/tests/character_routes.test.js
+++ src/tests/character_routes.test.js
@@ -1,8 +1,11 @@
 import request from 'supertest'
 import app from '../app'
 import RickAndMortyApiService from '../services/rick_and_morty_api_service'
+import models from '../models'
+const { ValidationError } = require('sequelize')
 
 jest.mock('../services/rick_and_morty_api_service')
+jest.mock('../models')
 
 describe('Test getting N characters', () => {
   beforeEach(() => {
@@ -57,6 +60,89 @@ describe('Test getting N characters', () => {
       .then(response => {
         expect(response.statusCode).toBe(200)
         expect(response.body.results).toHaveLength(25)
+      })
+  })
+})
+
+describe('Test creating characters', () => {
+  test('Test creating a character successfully', () => {
+    jest.spyOn(models.Character, 'findOrCreate').mockImplementation(() => {
+      return [{}, true]
+    })
+
+    return request(app)
+      .post('/character')
+      .send({
+        name: 'Rick',
+        status: 'Alive',
+        species: 'Human',
+        origin: 'Earth'
+      })
+      .then(response => {
+        expect(response.statusCode).toBe(200)
+        expect(models.Character.findOrCreate).toHaveBeenCalledWith({
+          where: { name: 'Rick' },
+          defaults: {
+            status: 'Alive',
+            species: 'Human',
+            origin: 'Earth'
+          }
+        })
+      })
+  })
+
+  test('Test validation error', () => {
+    jest.spyOn(models.Character, 'findOrCreate').mockImplementation(() => {
+      const error = new ValidationError()
+      error.errors = [{ message: 'Validation error' }]
+      throw error
+    })
+
+    return request(app)
+      .post('/character')
+      .send({
+        name: 'Rick',
+        status: 'Alive',
+        species: 'Human',
+        origin: 'Earth'
+      })
+      .then(response => {
+        expect(response.statusCode).toBe(400)
+      })
+  })
+
+  test('Test general error', () => {
+    jest.spyOn(models.Character, 'findOrCreate').mockImplementation(() => {
+      throw new Error()
+    })
+
+    return request(app)
+      .post('/character')
+      .send({
+        name: 'Rick',
+        status: 'Alive',
+        species: 'Human',
+        origin: 'Earth'
+      })
+      .then(response => {
+        expect(response.statusCode).toBe(500)
+      })
+  })
+
+  test('Test character already exists', () => {
+    jest.spyOn(models.Character, 'findOrCreate').mockImplementation(() => {
+      return [{}, false]
+    })
+    return request(app)
+      .post('/character')
+      .send({
+        name: 'Rick',
+        status: 'Alive',
+        species: 'Human',
+        origin: 'Earth'
+      })
+      .then(response => {
+        expect(response.statusCode).toBe(400)
       })
   })
 })
