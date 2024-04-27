bcfe231c56d7fd607aefb2752399f61e894ab6c3
Daniel Rojas <danielrc14@gmail.com>
2024-04-26 22:28:22 -0400

    chore: separate app for testing purposes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-import express from 'express'
+import app from './app'
 
-import './config/environment'
-import routes from './routes'
-import './models'
-
-const app = express()
 const port = process.env.PORT || 5000
-
-app.use(express.json())
-app.use('/', routes)
 
 const startServer = () => {
   app.listen(port, () => {
